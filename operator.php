afcc7188f121a58b9e6f3cd863fff60ffda78076
azriel122090153 <muhamadazrielakbar@smkwikrama.sch.id>
2023-05-10 13:33:29 +0700

    ajil

---
+++ operator.php
@@ -0,0 +1,142 @@
+<?php
+$a1 = 89;
+$a2 = 79;
+$akes = $a1 + $a2;
+
+echo "penjumahan = ". $akes;
+echo "<br>";    
+
+?>
+
+
+<?php
+$angka1 = 298;
+$angka2 = 67 ;
+$angkatotal = $angka1 - $angka2;
+
+echo "pengurangan = ". $angkatotal;
+echo "<br>";
+
+?>
+
+
+<?php
+
+$A1 = 78;
+$A2 = 21;
+$total = $A1 * $A2;
+
+echo "perkalian = ". $total;
+echo "<br>";
+
+?>
+
+<?php
+
+$a1 = 1258;
+$a2 = 2 ;
+$total = $a1 / $a2;
+
+echo "pembagian = ". $total;
+echo "<br>";
+
+?>
+
+<?php
+
+$A1 = 100;
+$a2 = 10;
+$TOTAL = $A1 % $A2;
+
+echo "modulus = ". $TOTAL;
+echo "<br>";
+?>
+
+<?php 
+
+$x =0;
+$x++;
+
+echo "increment = ". $x;
+echo "<br>";
+?>
+
+<?php
+$x = 100;
+$x--;
+
+echo "dicrement =". $x--;
+echo "<br>";
+?>   
+
+
+<?php
+
+
+
+$a = 9;
+$b = 3;
+
+?>
+
+<?php
+
+$c = $a > $b;
+echo "lebih besar =" . "$a > $b: $c";
+echo "<br>";
+
+?>
+
+<?php
+$c = $a < $b;
+echo "lebih kecil =". "$a < $b: $c";
+echo "<br>";
+?>
+
+<?php
+
+1 > 10;
+
+$A = "21";
+$B = 21;
+$A === $B;
+
+$c = $a == $b;
+echo "sama dengan =". "$a == $b: $c";
+echo "<br>";
+?>
+
+<?php
+$c = $a != $b;
+echo "tidak sama dengan =" . "$a != $b: $c";
+echo "<br>";
+?>
+
+<?php
+
+$c = $a >= $b;
+echo"lebih besar sama dengan =" . "$a >= $b: $c";
+echo "<br>";
+?>
+
+<?Php
+$c = $a <= $b;
+echo "lebih kecil sama dengan =". "$a <= $b: $c";
+echo "<br>";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tugas1.php
@@ -0,0 +1,29 @@
+<?php
+
+echo  "<center> <h1>operator perbandingan</h1> <center>";
+echo "<h3>contoh lebih besar </h3>";
+echo "<h3> 75 > 25 </h3>";
+var_dump(75 > 25); echo "<hr>";
+
+echo "<h3>contoh lebih kecil</h3>";
+echo "<h3> 10 < 25 </h3>";
+var_dump(10 < 25); echo "<hr>";
+
+echo "<h3>contoh sama dengan</h3>";
+echo "<h3> 45 == 25 </h3>";
+var_dump(45 == 25); echo "<hr>";
+
+echo "<h3>contoh tidak sama dengan </h3>";
+echo "<h3>  30 != 25 </h3>";
+var_dump(30 != 25); echo "<hr>";
+
+echo "<h3>contoh lebih besar sama dengan</h3>";
+echo "<h3> 20 >= 25 </h3>";
+var_dump(20 >= 25); echo "<hr>";
+
+echo "<h3>contoh lebih kecil sama dengan</h3>";
+echo "<h3>60 <= 25 </h3>";
+var_dump(60 <= 25); echo "<hr>";
+
+
+?>
